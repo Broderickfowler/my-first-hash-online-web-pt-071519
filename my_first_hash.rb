7db890c58514c2cbe1f2691e0f771d829f94f5b9
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-03 13:00:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,12 +5,12 @@ my_hash = { "name" => "", "place" => ""}
 end
 
 
-def shipping_manifest(the_manifest)
+def shipping_manifest(old-timey)
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
-the_manifest = { "whale bone corsets" => 5, "porcelain" => 2, "oil paintings" => 3}
+old-timey = { "whale bone corsets" => 5, "porcelain" => 2, "oil paintings" => 3}
 
 end
 
